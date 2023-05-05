5c732ea024ee1c2feb68b64163c33f2b2c106d73
Noise-Wall <tech.dscar.mx@gmail.com>
2023-05-04 17:11:24 -0700

    RateLimit.js

---
+++ server/src/controllers/rateLimit.js
@@ -0,0 +1,27 @@
+const rateLimit = require("express-rate-limit");
+
+// limita los intentos fallidos de inicio de sesion.
+const loginLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000,
+  max: 5,
+  message: {
+    message:
+      "Han habido demasiados intentos de inicio de sesión fallidos desde esta IP. Favor de intentarlo más tarde.",
+  },
+  skipSuccesfulRequests: true,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+// limita peticiones por cada segundo y medio.
+const stdRateLimiter = rateLimit({
+  windowMs: 1500,
+  max: 3,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+module.exports = {
+  loginLimiter,
+  stdRateLimiter,
+};
